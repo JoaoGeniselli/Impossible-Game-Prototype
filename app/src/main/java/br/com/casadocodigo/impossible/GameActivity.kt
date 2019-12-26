d2c87b9653fde2fc43a3deabb42e8d5c6e23a7c9
JoaoGeniselli <joao.g.geniselli@gmail.com>
2019-12-26 09:51:38 -0300

    Prototype implementation.

--- app/src/main/java/br/com/casadocodigo/impossible/GameActivity.kt
+++ app/src/main/java/br/com/casadocodigo/impossible/GameActivity.kt
@@ -1,31 +1,41 @@
 package br.com.casadocodigo.impossible
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
+import android.view.View
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(), View.OnTouchListener {
 
     private lateinit var view: ImpossibleSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         view = ImpossibleSurfaceView(this)
+        view.setOnTouchListener(this)
         setContentView(view)
-        setAsFullscreen()
-    }
-
-    private fun setAsFullscreen() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
     }
 
     override fun onResume() {
         super.onResume()
         view.onResume()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        event?.let {
+            if (event.x < 100 && event.y > 290 && event.y < 310) {
+                view.initGame()
+            }
+
+            if (event.x < 100 && event.y > 490 && event.y < 510) {
+                System.exit(0)
+            }
+        }
+
+        with(view) {
+            movePlayerDown(10f)
+            addScore(100)
+        }
+        return true
     }
 }
--- app/src/main/java/br/com/casadocodigo/impossible/ImpossibleSurfaceView.kt
+++ app/src/main/java/br/com/casadocodigo/impossible/ImpossibleSurfaceView.kt
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.impossible
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
@@ -12,27 +13,113 @@ class ImpossibleSurfaceView(context: Context) : SurfaceView(context), Runnable {
     private val surfaceHolder: SurfaceHolder = holder
     private val paint = Paint()
 
+    private var playerX = 300f
+    private var playerY = 300f
+    private var playerRadius = 50f
+
+    private var enemyX = 50f
+    private var enemyY = 50f
+    private var enemyRadius = 50f
+
+    private var score = 0
+
+    private var distance: Double = 0.0
+    private var gameOver = false
+
     private var running = false
     private lateinit var renderThread: Thread
+
+    fun initGame() {
+        enemyX = 0f
+        enemyY = 0f
+        enemyRadius = 0f
+        playerY = 300f
+        playerX = 300f
+        playerRadius = 50f
+        score = 0
+        gameOver = false
+    }
 
     fun onResume() {
         running = true
         renderThread = Thread(this)
-        renderThread.run()
+        renderThread.start()
     }
 
     override fun run() {
         while (running) {
             if (!surfaceHolder.surface.isValid) continue
-
             val canvas = surfaceHolder.lockCanvas()
+            canvas.drawBitmap(BitmapFactory.decodeResource(resources, R.drawable.sky), 0f, 0f, null)
             drawPlayer(canvas)
+            drawEnemy(canvas)
+            checkCollision(canvas)
+            if (gameOver) stopGame(canvas)
+            drawScore(canvas)
+            drawButtons(canvas)
             surfaceHolder.unlockCanvasAndPost(canvas)
+
+            if (gameOver) break
         }
     }
 
     private fun drawPlayer(canvas: Canvas) {
-        paint.color = Color.GREEN
-        canvas.drawCircle(100f, 100f, 100f, paint)
+        canvas.drawBitmap(
+            BitmapFactory.decodeResource(resources, R.drawable.nave),
+            playerX - 50f,
+            playerY - 50f,
+            null
+        )
+    }
+
+    private fun drawEnemy(canvas: Canvas) {
+        paint.color = Color.RED
+        enemyRadius++
+        canvas.drawCircle(enemyX, enemyY, enemyRadius, paint)
+    }
+
+    fun movePlayerDown(pixels: Float) {
+        playerY += pixels
+    }
+
+    private fun checkCollision(canvas: Canvas) {
+        val yAxis = Math.pow(playerY.toDouble() - enemyY.toDouble(), 2.0)
+        val xAxis = Math.pow(playerX.toDouble() - enemyX.toDouble(), 2.0)
+        distance = Math.sqrt(yAxis + xAxis)
+
+        if (distance <= playerRadius + enemyRadius) {
+            gameOver = true
+        }
+    }
+
+    private fun stopGame(canvas: Canvas) {
+        paint.style = Paint.Style.FILL
+        paint.color = Color.BLUE
+        paint.textSize = 100f
+        canvas.drawText("GAME	OVER!", 50f, 150f, paint)
+    }
+
+    fun addScore(points: Int) {
+        score += points
+    }
+
+    private fun drawScore(canvas: Canvas) {
+        paint.style = Paint.Style.FILL
+        paint.color = Color.WHITE
+        paint.textSize = 50f
+        canvas.drawText(score.toString(), 50f, 200f, paint)
+    }
+
+    private fun drawButtons(canvas: Canvas) {
+        //	Restart
+        paint.style = Paint.Style.FILL
+        paint.color = Color.WHITE
+        paint.textSize = 30f
+        canvas.drawText("Restart", 50f, 300f, paint)
+        //	Exit
+        paint.style = Paint.Style.FILL
+        paint.color = Color.WHITE
+        paint.textSize = 30f
+        canvas.drawText("Exit", 50f, 500f, paint)
     }
 }
